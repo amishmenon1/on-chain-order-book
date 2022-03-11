877388cc7b913b53ee564a32103aa2e52640ffe2
Amish Menon <amishmenon@gmail.com>
2022-03-10 19:31:35 -0600

    Updates

---
+++ precache-manifest.0287b85a2ab0c21c696fd3038eeab648.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a379883d351348dfbf40",
+    "url": "/on-chain-order-book/static/css/main.62f0992c.chunk.css"
+  },
+  {
+    "revision": "a379883d351348dfbf40",
+    "url": "/on-chain-order-book/static/js/main.a379883d.chunk.js"
+  },
+  {
+    "revision": "1614c3aceb534072c30e",
+    "url": "/on-chain-order-book/static/css/1.f688b830.chunk.css"
+  },
+  {
+    "revision": "1614c3aceb534072c30e",
+    "url": "/on-chain-order-book/static/js/1.1614c3ac.chunk.js"
+  },
+  {
+    "revision": "cd808d7f8458df41c4b6",
+    "url": "/on-chain-order-book/static/js/runtime~main.cd808d7f.js"
+  },
+  {
+    "revision": "187cdcc72ee21aa64d50a8b83c53993c",
+    "url": "/on-chain-order-book/static/media/chainlink-link-logo.187cdcc7.png"
+  },
+  {
+    "revision": "3f4b54c2dc843385d96f121d57b3531b",
+    "url": "/on-chain-order-book/static/media/order-book-logo.3f4b54c2.png"
+  },
+  {
+    "revision": "45e740b56c4176251d617006c9f1979b",
+    "url": "/on-chain-order-book/index.html"
+  }
+];
